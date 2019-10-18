faf2e5e0efa031ce7e9e1e4ad5081c28ba7b6cf9
Andrew Pearson <andrew@apearson.io>
2019-10-17 19:10:18 -0500

    Upgrading to stable version of packet parser

--- src/devices/DimmableLightingDevices/KeypadDimmer.ts
+++ src/devices/DimmableLightingDevices/KeypadDimmer.ts
@@ -1,6 +1,23 @@
 /* Libraries */
 import DimmableLightingDevice from './DimmableLightingDevice';
+import { Packet } from 'insteon-packet-parser';
 
 /* Class */
 export default class KeypadDimmer extends DimmableLightingDevice {
+
+  public async getDeviceStatus(){
+
+    const status = await super.getDeviceStatus();
+
+    const ledStatus = await this.sendInsteonCommand(0x2E, 0x00, [0x00, 0x00]) as Packet.ExtendedMessageRecieved;
+
+    // console.log(ledStatus);
+
+    return {
+      level: status.level,
+      // led: ledStatus.extendedData[9],
+    }
+
+  }
+
 }
