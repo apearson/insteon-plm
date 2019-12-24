7ae5331195c5917492dbdce09240d4bb57bad41f
Paul Wieland <p@ul.sickdimension.com>
2019-12-24 11:24:14 -0500

    Updated event emitter to keep consistency with dimmable device class

--- src/devices/SecurityDevice/LeakSensor.ts
+++ src/devices/SecurityDevice/LeakSensor.ts
@@ -7,16 +7,18 @@ export default class LeakSensor extends SecurityDevice {
 	public setupEvents(){
 		/* InsteonDevice emits all packets with type & subtype
 		   type 0x50 = Standard Message Received
-		   subtype 0x06 = Broadcast (Physically Triggered)
+		   subtype 0x06 = Broadcast (Physically Triggered) when the from address matches this device.
 		 */
-		this.on(['p', PacketID.StandardMessageReceived.toString(16), MessageSubtype.GroupBroadcastMessage.toString(16)], (data: Packet.StandardMessageRecieved) => {
-			switch(true){
-				case data.cmd1 === 0x11 && data.cmd2 === 0x01: this.emitPhysical(['sensor','dry'], data); break; // Doesn't trigger automatically. You have to click the set button to reset after water is detected
-				case data.cmd1 === 0x11 && data.cmd2 === 0x02: this.emitPhysical(['sensor','wet'], data); break;
-				case data.cmd1 === 0x11 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','dry'], data); break; // not tested
-				case data.cmd1 === 0x13 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','wet'], data); break; // not tested
-				// default: console.log("Unknown Leak Broadcast command",data.cmd1,data.cmd2);
-			}
-		});
+		this.on(['p', PacketID.StandardMessageReceived.toString(16), MessageSubtype.GroupBroadcastMessage.toString(16)], this.physicalEventEmitter);
+	}
+	
+	private physicalEventEmitter(data: Packet.StandardMessageRecieved){
+		switch(true){
+			case data.cmd1 === 0x11 && data.cmd2 === 0x01: this.emitPhysical(['sensor','dry'], data); break; // Doesn't trigger automatically. You have to click the set button to reset after water is detected
+			case data.cmd1 === 0x11 && data.cmd2 === 0x02: this.emitPhysical(['sensor','wet'], data); break;
+			case data.cmd1 === 0x11 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','dry'], data); break; // not tested
+			case data.cmd1 === 0x13 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','wet'], data); break; // not tested
+			// default: console.log("Unknown Leak Broadcast command",data.cmd1,data.cmd2);
+		}
 	}
 }
--- src/devices/SecurityDevice/OpenCloseSensor.ts
+++ src/devices/SecurityDevice/OpenCloseSensor.ts
@@ -7,18 +7,20 @@ export default class OpenCloseSensor extends SecurityDevice {
 	public setupEvents(){
 		/* InsteonDevice emits all packets with type & subtype
 		   type 0x50 = Standard Message Received
-		   subtype 0x06 = Broadcast (Physically Triggered)
+		   subtype 0x06 = Broadcast (Physically Triggered) when the from address matches this device.
 		 */
-		this.on(['p', PacketID.StandardMessageReceived.toString(16), MessageSubtype.GroupBroadcastMessage.toString(16)], (data: Packet.StandardMessageRecieved) => {
-			switch(true){
-				case data.cmd1 === 0x11 && data.cmd2 === 0x01: this.emitPhysical(['sensor','opened'], data); break;
-				case data.cmd1 === 0x13 && data.cmd2 === 0x01: this.emitPhysical(['sensor','closed'], data); break;
-				case data.cmd1 === 0x11 && data.cmd2 === 0x03: this.emitPhysical(['battery','opened'], data); break; // not tested
-				case data.cmd1 === 0x13 && data.cmd2 === 0x03: this.emitPhysical(['battery','closed'], data); break; // not tested
-				case data.cmd1 === 0x11 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','opened'], data); break; // not tested
-				case data.cmd1 === 0x13 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','closed'], data); break; // not tested
-				// default: console.log("Unknown Broadcast command",data.cmd1,data.cmd2);
-			}
-		});
+		this.on(['p', PacketID.StandardMessageReceived.toString(16), MessageSubtype.GroupBroadcastMessage.toString(16)], this.physicalEventEmitter);
+	}
+	
+	private physicalEventEmitter(data: Packet.StandardMessageRecieved){
+		switch(true){
+			case data.cmd1 === 0x11 && data.cmd2 === 0x01: this.emitPhysical(['sensor','opened'], data); break;
+			case data.cmd1 === 0x13 && data.cmd2 === 0x01: this.emitPhysical(['sensor','closed'], data); break;
+			case data.cmd1 === 0x11 && data.cmd2 === 0x03: this.emitPhysical(['battery','opened'], data); break; // not tested
+			case data.cmd1 === 0x13 && data.cmd2 === 0x03: this.emitPhysical(['battery','closed'], data); break; // not tested
+			case data.cmd1 === 0x11 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','opened'], data); break; // not tested
+			case data.cmd1 === 0x13 && data.cmd2 === 0x04: this.emitPhysical(['heartbeat','closed'], data); break; // not tested
+			// default: console.log("Unknown Broadcast command",data.cmd1,data.cmd2);
+		}
 	}
 }
